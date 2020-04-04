3ab0597df5d9ac0f385205a7429f94df5b90f477
Kamil Ślósarczyk <48627674+BlackSlusarz@users.noreply.github.com>
2020-04-04 10:34:28 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,54 +1,22 @@
 
+
+let installButton = document.getElementById('install');
+
 if ('serviceWorker' in navigator) {
-
-    navigator.serviceWorker
-        .register('./serviceWorker.js', { scope: './' })
-        .then(function(registration) {
-            console.log("Service Worker Registered. Scope is:"+registration.scope);
-        })
-        .catch(function(err) {
-            console.log("Service Worker Failed to Register", err);
-        })
-
-}
-
-
-let deferredPrompt;
-
-let btnAdd = document.getElementById("btnAdd");
-
-window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earli	er from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can b	e triggered later.
-    deferredPrompt = e;
-    // Update UI notify the user t	hey can add to home screen
-    let btnAdd = document.getElementById("btnAdd");
-    btnAdd.style.display = 'inline-block';
-    //btnAdd.style.border = 'solid 2px #e09636';
-    btnAdd.onclick = function() {
-        deferredPrompt.prompt();
-    };
-});
-
-// See https://developers.google.com/web/fundamentals/app-install-banners/
-btnAdd.addEventListener('click', (e) => {
-    // hide our user interface that shows our A2HS button
-    btnAdd.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice
-        .then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the A2HS prompt');
-            } else {
-                console.log('User dismissed the A2HS prompt');
-            }
-            deferredPrompt = null;
+    navigator.serviceWorker.register('serviceWorker.js').then(() => {
+        return navigator.serviceWorker.ready;
+    }).then(reg => {
+        console.log('Service Worker ready :^)', reg);
+    }).catch(error => {
+        console.log('Service Worker error :^(', error);
+    });
+    window.addEventListener('beforeinstallprompt', event => {
+        event.preventDefault();
+        console.log('beforeinstallprompt event:', event);
+        installButton.removeAttribute('hidden');
+        installButton.addEventListener('click', () => {
+            event.prompt();
+            installButton.setAttribute('disabled', true);
         });
-});
-window.addEventListener('appinstalled', (evt) => {
-    // For debug purposes only!
-    console.log('a2hs installed');
-});
+    });
+}
