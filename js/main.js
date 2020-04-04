2a6697d90ecc8087d56e6d37f69a41ac23adf610
Kamil Ślósarczyk <48627674+BlackSlusarz@users.noreply.github.com>
2020-04-04 10:07:54 +0200

    Update main.js
    Added prompt to install for user

--- js/main.js
+++ js/main.js
@@ -12,3 +12,43 @@ if ('serviceWorker' in navigator) {
 
 }
 
+
+let deferredPrompt;
+
+let btnAdd = document.getElementById("btnAdd");
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earli	er from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can b	e triggered later.
+    deferredPrompt = e;
+    // Update UI notify the user t	hey can add to home screen
+    let btnAdd = document.getElementById("btnAdd");
+    btnAdd.style.display = 'inline-block';
+    //btnAdd.style.border = 'solid 2px #e09636';
+    btnAdd.onclick = function() {
+        deferredPrompt.prompt();
+    };
+});
+
+// See https://developers.google.com/web/fundamentals/app-install-banners/
+btnAdd.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    btnAdd.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice
+        .then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+            } else {
+                console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+        });
+});
+window.addEventListener('appinstalled', (evt) => {
+    // For debug purposes only!
+    console.log('a2hs installed');
+});
